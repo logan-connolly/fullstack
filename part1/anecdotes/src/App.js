983f8a48411e2761c180fb0863ac7f4810642e39
Logan Connolly <logan.connolly@protonmail.com>
2021-10-12 00:11:37 +0200

    Add solution to part1 anecdotes

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const Header = ({ text }) => <h1>{text}</h1>;
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
+const Anecdote = ({ text, votes }) => {
+  return (
+    <div>
+      <p>"{text}"</p>
+      <p>{votes} votes</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(() => {
+    return new Array(anecdotes.length).fill(0);
+  });
+
+  const best = votes.reduce(
+    (bestIndex, currentCount, currentIndex, votes) =>
+      currentCount > votes[bestIndex] ? currentIndex : bestIndex,
+    0
+  );
+
+  const getRandomAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    if (selected === randomIndex) getRandomAnecdote();
+    setSelected(randomIndex);
+  };
+
+  const voteForAnecdote = () => {
+    let updatedVotes = [...votes];
+    updatedVotes[selected] += 1;
+    setVotes(updatedVotes);
+  };
+
+  return (
+    <>
+      <Header text="Anecdote of the day" />
+      <Anecdote text={anecdotes[selected]} votes={votes[selected]} />
+      <Button text="vote" onClick={voteForAnecdote} />
+      <Button text="next anecdote" onClick={getRandomAnecdote} />
+      <Header text="Anecdote with most votes" />
+      <Anecdote text={anecdotes[best]} votes={votes[best]} />
+    </>
+  );
+};
+
+export default App;
