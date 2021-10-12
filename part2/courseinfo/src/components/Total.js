dd66bd148100db9747df6c055e199f3f69fc0170
Logan Connolly <logan.connolly@protonmail.com>
2021-10-12 20:19:40 +0200

    Initial commit for part2 courseinfo

---
+++ part2/courseinfo/src/components/Total.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Total = ({ parts }) => {
+  const total = parts
+    .map((part) => part.exercises)
+    .reduce((acc, next) => acc + next, 0);
+
+  return <p style={{ fontWeight: "bold" }}>total of {total} exercises</p>;
+};
+
+export default Total;
