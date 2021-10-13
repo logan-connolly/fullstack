48f0a8bfcc7c6a6a31cdfc4aeb613b0327d5cbba
Logan Connolly <logan.connolly@protonmail.com>
2021-10-13 18:48:49 +0200

    feat(countries): fetch weather from api

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,19 +1,48 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Country = ({ country }) => {
+  const [currentWeather, setCurrentWeather] = useState({});
+  const weatherUrl = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`;
+
+  useEffect(() => {
+    axios.get(weatherUrl).then((resp) => {
+      setCurrentWeather(resp.data.current);
+    });
+  }, [weatherUrl, country.capital]);
+
   return (
-    <div>
-      <h1>
-        {country.name.common} {country.flag}
-      </h1>
-      <p>capital {country.capital}</p>
-      <p>population {country.population}</p>
-      <h4>languages</h4>
-      <ul>
-        {Object.entries(country.languages).map((item) => {
-          const [code, language] = item;
-          return <li key={code}>{language}</li>;
-        })}
-      </ul>
-    </div>
+    <>
+      <div>
+        <h1>
+          {country.name.common} {country.flag}
+        </h1>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+      </div>
+      <div>
+        <h4>languages</h4>
+        <ul>
+          {Object.entries(country.languages).map((item) => {
+            const [code, language] = item;
+            return <li key={code}>{language}</li>;
+          })}
+        </ul>
+      </div>
+      <div>
+        <h4>Weather in {country.capital}</h4>
+        <div>
+          <strong>temperature:</strong> {currentWeather.temperature}
+        </div>
+        <div>
+          <img src={currentWeather.weather_icons} alt="weather logo" />
+        </div>
+        <div>
+          <strong>wind:</strong> {currentWeather.wind_speed} mph direction{" "}
+          {currentWeather.wind_dir}
+        </div>
+      </div>
+    </>
   );
 };
 
--- part2/countries/src/components/CountryList.js
+++ part2/countries/src/components/CountryList.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const CountryList = ({ countries, search, setSearch, setSelected }) => {
   let filtered = countries.filter((country) =>
     country.name.common.includes(search)
@@ -9,29 +7,24 @@ const CountryList = ({ countries, search, setSearch, setSelected }) => {
     return <p>Too many matches, specify another filter</p>;
   }
 
-  const buttonHandler = (country) => {
+  const selectionHandler = (country) => {
     setSearch(() => country.name.common);
     setSelected(country);
   };
 
-  if (filtered.length > 1) {
+  if (filtered.length > 0) {
     return (
       <ul>
         {filtered.map((country) => (
           <li key={country.cca3}>
             {country.name.common}
             <span>
-              <button onClick={() => buttonHandler(country)}>show</button>
+              <button onClick={() => selectionHandler(country)}>show</button>
             </span>
           </li>
         ))}
       </ul>
     );
-  }
-
-  if (filtered.length === 1) {
-    const [country] = filtered;
-    setSelected(country);
   }
 
   return <p>Loading countries ...</p>;
